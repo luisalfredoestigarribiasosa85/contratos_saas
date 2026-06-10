aa0e3ff1b9488a742dfbb0be70def912454d4f6c
Luis Estigarribia <luisalfredoestigarribiasosa85@gmail.com>
2026-06-09 23:37:56 -0300

    Run production seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,10 @@ set -o errexit
 
 bundle install
 
+bundle exec rails db:migrate RAILS_ENV=production
+
+bundle exec rails db:seed RAILS_ENV=production
+
 bundle exec rails assets:precompile
+
 bundle exec rails assets:clean
