a6ca7f7d6768f61bb8cc63712bb41cd49c258813
Luis Estigarribia <luisalfredoestigarribiasosa85@gmail.com>
2026-06-09 23:41:19 -0300

    removed seed from render and render-build for not duplicate

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,10 +3,5 @@ set -o errexit
 
 bundle install
 
-bundle exec rails db:migrate RAILS_ENV=production
-
-bundle exec rails db:seed RAILS_ENV=production
-
 bundle exec rails assets:precompile
-
 bundle exec rails assets:clean
